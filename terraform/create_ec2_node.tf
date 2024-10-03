654566a285b82dafcdc0711cdfc3ab9b1b4b9490
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2024-10-03 16:00:00 +0530

    Update create_ec2_node.tf
    added dns hostname

--- terraform/create_ec2_node.tf
+++ terraform/create_ec2_node.tf
@@ -7,6 +7,9 @@ resource "aws_key_pair" "TF_To_EC2_key" {
 # Create VPC
 resource "aws_vpc" "tf_vpc" {
   cidr_block = "172.16.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
 
   tags = {
     Name = "tf-vpc"
