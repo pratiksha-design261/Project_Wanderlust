ec4374957321ac7fcc8715402124eabe8dad0124
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2024-10-05 04:41:57 +0530

    Update updateBackendNew.sh

--- Automations/updateBackendNew.sh
+++ Automations/updateBackendNew.sh
@@ -24,7 +24,7 @@ Current_url=$(sed -n "4p" $Env_file)
 #Check if both URL re same if not update new url to .env.docker file
 if [[ "${Current_url}" != "${Updated_BE_Url}" ]]; then
     if [ -f $Env_file ]; then                                                 # Checks if file is availabel on location
-        sed -i -e "s|FRONTEND_URL.*|${Updated_BE_Url}|g"$Env_file
+        sed -i -e "s|FRONTEND_URL.*|${Updated_BE_Url}|g" $Env_file
     else
         echo "Error: File Not Found."
     fi
