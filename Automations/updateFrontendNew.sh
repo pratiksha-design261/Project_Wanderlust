1fb091db10177708c71f55ff0aa7e7a3bd32fac5
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2024-10-05 04:31:25 +0530

    Update updateFrontendNew.sh

--- Automations/updateFrontendNew.sh
+++ Automations/updateFrontendNew.sh
@@ -2,7 +2,7 @@
 InstanceID="i-08207742f70c49793"
 
 #Get IP address from Instnace ID for frontend with instance id of EC2
-IPV4_ID_FE=$(aws ec2 describe-instances --instance-ids=$InstanceID --query 'Reservations[0].Instance[0].PublicIpAddress' --output text)
+IPV4_ID_FE=$(aws ec2 describe-instances --instance-ids $InstanceID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 
 # File where we need to update IPV4 address
 env_file_path="../frontend/.env.docker"
@@ -16,7 +16,7 @@ current_url=$(cat $env_file_path)
 # compare current url and updated url, if its diffrent update in env.docker file
 if [[ ${current_url} != ${Updated_FE_Url} ]]; then
     if [ -f $env_file_path ]; then
-        sed -i -e "s|VITE_API_PATH.|${Updated_FE_Url}|g"$env_file_path
+        sed -i -e "s|VITE_API_PATH.*|${Updated_FE_Url}|g"$env_file_path
     else
         echo "Error: File not found"
     fi
