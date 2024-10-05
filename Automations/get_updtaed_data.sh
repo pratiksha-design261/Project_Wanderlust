f6ab36f7265861948ce9403f550c8ebbf3a9e3a1
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2024-10-05 12:28:26 +0530

    Create get_updtaed_data.sh

---
+++ Automations/get_updtaed_data.sh
@@ -0,0 +1,10 @@
+# Backend Updated data validate
+Env_file="../backend/.env.docker"
+Current_url=$(sed -n "4p" $Env_file)
+echo "Backend : ${Current_url}"
+
+
+# Front end Updated data validate
+env_file_path="../frontend/.env.docker"
+current_url_1=$(cat $env_file_path)
+echo "FrontEnd: ${Current_url_1}"
